195073aa91d2fb2346927b6341f6979e13391f07
AshakaE <ashakaegerega@gmail.com>
2023-10-01 11:45:59 +0100

    Add graphql graphene-django

--- django_graphql/settings.py
+++ django_graphql/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    "graphene_django",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema",
+}
 
 ROOT_URLCONF = 'django_graphql.urls'
 
